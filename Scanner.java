b5cebca73d59f9e522e3b318c8c40e228ab554b8
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-28 19:01:07 +0530

    lexical analysis

--- Scanner.java
+++ Scanner.java
@@ -9,8 +9,75 @@ import static com.craftinginterpreters.lox.TokenType.*;
 class Scanner {
     private final String source;
     private final List<Token> tokens = new ArrayList<>();
-    
+
+    private int start = 0;   // points to the first character in the lexeme
+    private int current = 0; // points at the character currently being considered
+    private int line = 1;    // tracks what source line `current` is on
+
     Scanner(String source) {
         this.source = source;
     }
+}
+
+List<Token> scanTokens(){
+    while (!isAtEnd()) {
+        // We are at the beginning of the next lexeme.
+        start = current;
+        scanToken();
+    }
+
+    tokens.add(new Token(EOF, "", null, line));
+    return tokens;
+}
+
+private void scanToken() {
+    char c = advance();
+
+    switch (c) {
+        case '(': addToken(LEFT_PAREN); break;
+        case ')': addToken(RIGHT_PAREN); break;
+        case '{': addToken(LEFT_BRACE); break;
+        case '}': addToken(RIGHT_BRACE); break;
+        case ',': addToken(COMMA); break;
+        case '.': addToken(DOT); break;
+        case '-': addToken(MINUS); break;
+        case '+': addToken(PLUS); break;
+        case ';': addToken(SEMICOLON); break;
+        case '*': addToken(STAR); break;
+
+        case '!': addToken(match('=') ? BANG_EQUAL : BANG); break;
+        case '=': addToken(match('=') ? EQUAL_EQUAL : EQUAL); break;
+        case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
+        case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
+
+        default: Lox.error(line, "Unexpected character."); break;
+    }
+}
+
+private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+    
+    current++;
+    return true;
+}
+
+private boolean isAtEnd() {
+    return current >= source.length();
+}
+
+// advance() method consumes the next character in the source file and returns it
+private char advance() {
+    current++;
+    return source.charAt(current - 1);
+}
+
+// grabs the text of the current lexeme and creates a new token
+private void addToken(TokenType type) {
+    addToken(type, null);
+}
+
+private void addToken(TokenType type, Object literal) {
+    String text = source.substring(start, current);
+    tokens.add(new Token(type, text, literal, line));
 }
