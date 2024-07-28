db0f56b0474141b04365683b45fb7e7811a62523
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-28 19:08:55 +0530

    lexical error

--- Scanner.java
+++ Scanner.java
@@ -50,16 +50,60 @@ private void scanToken() {
         case '<': addToken(match('=') ? LESS_EQUAL : LESS); break;
         case '>': addToken(match('=') ? GREATER_EQUAL : GREATER); break;
 
+        case '/':
+            if (match('/')) {
+            // A comment goes until the end of the line.
+            while (peek() != '\n' && !isAtEnd()) advance();
+            } else {
+            addToken(SLASH);
+            }
+            break;
+
+        case ' ':
+        case '\r':
+        case '\t':
+            // Ignore whitespace.
+            break;
+        case '\n':
+            line++;
+            break;
+
+        case '"': string(); break;
+
         default: Lox.error(line, "Unexpected character."); break;
     }
+}
+
+private void string() {
+    while (peek() != '"' && !isAtEnd()) {
+        if (peek() == '\n') line++;
+        advance();
+    }
+
+    if (isAtEnd()) {
+        Lox.error(line, "Unterminated string.");
+        return;
+    }
+    
+    // The closing ".
+    advance();
+    
+    // Trim the surrounding quotes.
+    String value = source.substring(start + 1, current - 1);
+    addToken(STRING, value);
 }
 
 private boolean match(char expected) {
     if (isAtEnd()) return false;
     if (source.charAt(current) != expected) return false;
-    
+
     current++;
     return true;
+}
+
+private char peek() {
+    if (isAtEnd()) return '\0';
+    return source.charAt(current);
 }
 
 private boolean isAtEnd() {
