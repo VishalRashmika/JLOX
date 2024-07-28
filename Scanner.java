bc0a3fda30f81be17a33e6f1f8125995f4068221
VishalRashmika <vishal.rashmika.perera@gmail.com>
2024-07-28 19:15:36 +0530

    number literals

--- Scanner.java
+++ Scanner.java
@@ -70,8 +70,27 @@ private void scanToken() {
 
         case '"': string(); break;
 
-        default: Lox.error(line, "Unexpected character."); break;
+        default: 
+            if (isDigit(c)) {
+                number();
+            } else {
+                Lox.error(line, "Unexpected character.");
+            }
+            break;
     }
+}
+
+private void number() {
+    while (isDigit(peek())) advance();
+
+    // Look for a fractional part.
+    if (peek() == '.' && isDigit(peekNext())) {
+        // Consume the "."
+        advance();
+        while (isDigit(peek())) advance();
+    }
+
+    addToken(NUMBER,Double.parseDouble(source.substring(start, current)));
 }
 
 private void string() {
@@ -104,6 +123,10 @@ private boolean match(char expected) {
 private char peek() {
     if (isAtEnd()) return '\0';
     return source.charAt(current);
+}
+
+private boolean isDigit(char c) {
+    return c >= '0' && c <= '9';
 }
 
 private boolean isAtEnd() {
